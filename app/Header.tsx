7267b3088d2ee6c6046da41b165e64da34d0f444
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-03 13:16:00 -0800

    Header.tsx is modified with Bars3Icon and Subscribe now button and styled

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,10 +1,22 @@
 import { Bars3Icon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
 
 function Header() {
   return (
     <header>
-      <div>
+      <div className='grid grid-cols-3 p-10 items-center'>
         <Bars3Icon className='h-8 w-8 cursor-pointer' />
+        <Link href='/' prefetch={false}>
+          <h1>The ES News</h1>
+        </Link>
+
+        <div>
+          {/* DarkModeButton */}
+
+          <button className='hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800'>
+            Subscribe Now
+          </button>
+        </div>
       </div>
     </header>
   );
