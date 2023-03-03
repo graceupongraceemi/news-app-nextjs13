79c959ec8f2400bd4559515e477119f518d3b4c9
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-03 00:02:54 -0800

    @heroicons/react installed and imported to Header.tsx of app server directory

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Bars3Icon } from '@heroicons/react/24/solid';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <Bars3Icon className='h-8 w-8 cursor-pointer' />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
