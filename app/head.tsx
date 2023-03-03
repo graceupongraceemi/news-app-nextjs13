4dec4f5bd7fc76cc2cc57aa9d021e7bb27117466
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-02 22:26:06 -0800

    head.tsx and Header.tsx added to app server directory

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>ES News</title>
+      <meta
+        content='width=device-width,
+      initial-scale=1'
+        name='viewport'
+      />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
