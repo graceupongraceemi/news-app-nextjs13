7de99879509e5b3ee8b0557792b16f6f8610eee6
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-03 19:09:59 -0800

    Navlink.tsx and Navlinks.tsx done

--- app/Header.tsx
+++ app/Header.tsx
@@ -26,7 +26,6 @@ function Header() {
         </div>
       </div>
 
-      {/* NavLinks */}
       <NavLinks />
       {/* SearchBox */}
     </header>
--- app/NavLinks.tsx
+++ app/NavLinks.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+// using below converts to client component
+'use client';
+
 import { categories } from '../constants';
+import { usePathname } from 'next/navigation';
 import NavLink from './NavLink';
+import { is } from 'immer/dist/internal';
 
 function NavLinks() {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    return pathname?.split('/').pop() === path;
+  };
+
+  // const isActive = (path: string) => {}
   return (
-    <nav>
+    <nav className='grid grid-cols-4 md:grid-cols-7 text-xs md:text-sm gap-4 ph-10 max-w-6xl mx-auto border-b'>
       {categories.map((category) => (
-        <NavLink key={category} category={category} isActive={true} />
+        <NavLink
+          key={category}
+          category={category}
+          isActive={isActive(category)}
+        />
       ))}
     </nav>
   );
