90732ea200077099efb3570362c04bb568d48ac3
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-04 17:55:45 -0800

    lib directory created and fetchNews.ts added. graphql-request graphql installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { categories } from '../constants';
+import fetchNews from '../lib/fetchNews';
+
 async function Homepage() {
   // fetch the news data
+  const news: NewsResponse = await fetchNews(categories.join(','));
 
   return <div>{/* NewsList news */}</div>;
 }
