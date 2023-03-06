16e910e4f90264591682688bd88bb5f2a237edcc
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-05 22:47:47 -0800

    layout.tsx and page.tsx modified. mediastack api added to .env.local

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import fetchNews from '../lib/fetchNews';
 
 async function Homepage() {
   // fetch the news data
-  const news: NewsResponse = await fetchNews(categories.join(','));
+  // const news: NewsResponse = await fetchNews(categories.join(','));
 
   return <div>{/* NewsList news */}</div>;
 }
