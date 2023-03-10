ca27fcde00c803db4bbbe03a64681943749760d5
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-09 19:35:13 -0800

    fetchNews.ts updated with await fetch method

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -41,6 +41,29 @@ const fetchNews = async (
     }
   `;
   // Fetch function with Next.js 13 caching...
+  const res = await fetch(
+    'https://haltern.stepzen.net/api/unrealistic-pug/__graphql',
+    {
+      method: 'POST',
+      cache: isDynamic ? 'no-cache' : 'default',
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords
+        }
+      })
+    }
+  );
+
+  console.log('LOADING NEW DATA FROM API for category >>>', category, keywords);
+
   // Sort function by images vs not images present
   //  return res
 };
