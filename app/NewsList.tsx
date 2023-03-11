e2d88b94034cab2fb8bfae78d4b58aa1e4fc0115
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-11 12:28:33 -0800

    created NewsList.tsx in app directory. page.tsx is updated with NewsList

---
+++ app/NewsList.tsx
@@ -0,0 +1,14 @@
+
+type Props = {
+  news: NewsResponse
+}
+
+function NewsList({new}: Props) {
+  return (
+    <main>
+      {/* All the articles */}
+    </main>
+  )
+}
+
+export default NewsList
