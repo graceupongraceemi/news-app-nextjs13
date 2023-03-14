a9066f93d5080a6c49f87c63abaec478a5082e7f
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-14 05:35:09 -0700

    Article function in Article.tsx modified with section and footer

--- app/Article.tsx
+++ app/Article.tsx
@@ -12,6 +12,21 @@ function Article({ article }: Props) {
           className='h-56 w-full object-cover rounded-t-lg shadow-md'
         />
       )}
+
+      <div>
+        <div>
+          <h2>{article.title}</h2>
+
+          <section>
+            <p>{article.description}</p>
+          </section>
+
+          <footer>
+            <p>{article.source}</p>
+            <p>{article.published_at}</p>
+          </footer>
+        </div>
+      </div>
     </article>
   );
 }
