0dc74b2627798968df999ac5b58479435496b77f
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-15 15:57:21 -0700

    response.json created. page.tsx in app directory modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { categories } from '../constants';
 import fetchNews from '../lib/fetchNews';
 import NewsList from './NewsList';
+import response from '../response.json';
 
 async function Homepage() {
   // fetch the news data
-  // const news: NewsResponse = await fetchNews(categories.join(','));
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(',')));
 
   return (
     <div>
