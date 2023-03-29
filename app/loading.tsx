572c2798674ab6ac6600a45681fd9587b3cd41e9
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-28 20:21:30 -0700

    loading.tsx file added to app directory

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+// function Loading() {
+//   return (
+//     <div className='animate-pulse font-serif text-lg text-gray-400 text-center p-10'>
+//       Loading New Feed...
+//     </div>
+//   );
+// }
